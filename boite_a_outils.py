b5a6c7d479c4d905eb0b2d0fe33febea062d238c
Arthus dg <galardarthus@gmail.com>
2025-05-05 14:57:43 +0200

    auto_nadir not found fixed and new option avalaible

--- boite_a_outils.py
+++ boite_a_outils.py
@@ -1,6 +1,5 @@
 import change_gps_2
 import kml_to_geojson
-import auto_nadir
 import subprocess as sp
 import os
 
@@ -41,9 +40,9 @@ def menu():
     choix = None
     while choix not in [1, 2, 3]:
         print(
-            "1- Go Pro max"
+            "1- GoPro max"
             "\n"
-            "2- Insta 360 one x2 \n"
+            "2- Insta360 one x2 \n"
             "3- Push panoramax OSM \n"
             "4- Exit"
         )
@@ -54,10 +53,10 @@ def menu():
             )
         )
     if choix == 1:
-        CAMERA = "Go Pro max"
+        CAMERA = "GoPro max"
         menu_go_pro()
     elif choix == 2:
-        CAMERA = "Insta 360 one x2"
+        CAMERA = "Insta360 one x2"
         menu_insta()
     elif choix == 3:
         loc_photos = None
@@ -90,18 +89,21 @@ def menu():
 
 def menu_go_pro():
     choix = None
-    while choix not in [1, 2, 3, 4]:
+    while choix not in [1, 2, 3, 4, 5]:
         print(
-            "1-Ajuster les coordonnées GPS \n"
-            "2-Ajouter un logo \n"
-            "3-Revenir au choix de camera \n"
-            "4-Exit"
+            "1-Mode auto \n"
+            "2-Ajuster les coordonnées GPS \n"
+            "3-Ajouter un logo \n"
+            "4-Revenir au choix de camera \n"
+            "5-Exit"
         )
         choix = int(input("Entrez le numero associé a l'action de votre choix: "))
     if choix == 1:
+        mode_auto()
+    elif choix == 2:
         change_gps_2.main()
         menu_go_pro()
-    elif choix == 2:
+    elif choix == 3:
         loc_photos = None
         while loc_photos not in [1, 2]:
             print("1- img_to_be_changed/ \n" "2- img_to_be_changed/changed/")
@@ -116,7 +118,7 @@ def menu_go_pro():
             path = "img_to_be_changed/changed/"
         sp.run(["./script_logo_insta.sh", path])
         menu_go_pro()
-    elif choix == 3:
+    elif choix == 4:
         menu()
     else:
         exit
@@ -176,6 +178,28 @@ def menu_insta():
         menu_insta()
     else:
         exit
+
+
+def mode_auto():
+    print(
+        "Bienvenue dans le mode auto cette option prendra tout en charge c'es à dire :\n"
+        "   -changement de coordonées gps \n"
+        "   -ajout d'un logo \n"
+        "   -modification des metadonné (si insta 360) \n"
+        "   -push sur panoramax \n"
+        "!!!! ATTENTION !!!! pour que ce programme se déroule bien veillez à bien avoir un fichier kml, les photos au format valide(2:1) et un logo"
+    )
+    kml_to_geojson.converter_kml()
+    change_gps_2.main()
+    sp.run(["./meta_insta.sh", "img_to_be_changed/changed/"])
+    if CAMERA == "Insta360 one x2":
+        sp.run(["./meta_insta.sh", "img_logo/"])
+    sp.run(
+        [
+            "panoramax_cli upload --api-url https://panoramax.openstreetmap.fr/",
+            "img_logo/",
+        ]
+    )
 
 
 menu()
