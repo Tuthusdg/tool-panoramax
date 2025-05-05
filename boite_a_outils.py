c963da0e97fe631349f464f31ee7fdf9845b2a4a
Arthus dg <galardarthus@gmail.com>
2025-05-05 15:06:31 +0200

    error and bug fix improve the security

--- boite_a_outils.py
+++ boite_a_outils.py
@@ -2,12 +2,14 @@ import change_gps_2
 import kml_to_geojson
 import subprocess as sp
 import os
+import sys
 
 CAMERA = ""
 
 
-print(
-    """developed by:
+def print_banner():
+    print(
+        """developed by:
                                                                        .                                     
                              .=====                                   
                       :==========:                                    
@@ -33,173 +35,176 @@ print(
    @@-   .@@  *@@ @@@@-  @@@@. :@@  @@.   .@@                         
     @@@@@  @@@@%  @@  @@ @@     -@@@@- @@@@@                          
                                                    """
-)
+    )
+
+
+def get_user_choice(prompt, options):
+    while True:
+        try:
+            choice = int(input(prompt))
+            if choice in options:
+                return choice
+            else:
+                print("Veuillez entrer un nombre valide.")
+        except ValueError:
+            print("Entrée invalide. Veuillez entrer un chiffre.")
 
 
 def menu():
-    choix = None
-    while choix not in [1, 2, 3]:
-        print(
-            "1- GoPro max"
-            "\n"
-            "2- Insta360 one x2 \n"
-            "3- Push panoramax OSM \n"
-            "4- Exit"
-        )
-        choix = int(
-            input(
-                "Entrez le numero associé a la camera "
-                "utilisé ou 3 si vous voulez arréter le programme: "
-            )
-        )
-    if choix == 1:
-        CAMERA = "GoPro max"
-        menu_go_pro()
-    elif choix == 2:
-        CAMERA = "Insta360 one x2"
-        menu_insta()
-    elif choix == 3:
-        loc_photos = None
-        while loc_photos not in [1, 2, 3]:
-            print(
-                "1-img_to_be_changed/ \n"
-                "2-img_to_be_changed/changed/ \n"
-                "3-img_logo/ \n"
-                "4-Exit"
-            )
-            loc_photos = int(
-                input(
-                    "Entré le nombre associez au chemin de vos photo ou 4 si vous voulez retourné au menu"
-                )
-            )
-        if loc_photos == 1:
-            path = "img_to_be_changed/"
-        elif loc_photos == 2:
-            path = "img_to_be_changed/changed/"
-        elif loc_photos == 3:
-            path = "img_logo/"
-        else:
-            menu()
+    global CAMERA
+
+    while True:
+        print("\n--- MENU PRINCIPAL ---")
+        print("1- GoPro max")
+        print("2- Insta360 one x2")
+        print("3- Push Panoramax OSM")
+        print("4- Quitter")
+
+        choix = get_user_choice("Votre choix: ", [1, 2, 3, 4])
+
+        if choix == 1:
+            CAMERA = "GoPro max"
+            menu_go_pro()
+        elif choix == 2:
+            CAMERA = "Insta360 one x2"
+            menu_insta()
+        elif choix == 3:
+            push_panoramax()
+        elif choix == 4:
+            print("Fin du programme.")
+            sys.exit()
+
+
+def push_panoramax():
+    print("\n--- Upload Panoramax ---")
+    print("1- img_to_be_changed/")
+    print("2- img_to_be_changed/changed/")
+    print("3- img_logo/")
+    print("4- Retour")
+
+    choix = get_user_choice("Choix du chemin photo: ", [1, 2, 3, 4])
+    path = {
+        1: "img_to_be_changed/",
+        2: "img_to_be_changed/changed/",
+        3: "img_logo/",
+    }.get(choix)
+
+    if path:
         sp.run(
-            ["panoramax_cli upload --api-url https://panoramax.openstreetmap.fr/", path]
+            [
+                "panoramax_cli",
+                "upload",
+                "--api-url",
+                "https://panoramax.openstreetmap.fr/",
+                path,
+            ]
         )
-    else:
-        exit
 
 
 def menu_go_pro():
-    choix = None
-    while choix not in [1, 2, 3, 4, 5]:
-        print(
-            "1-Mode auto \n"
-            "2-Ajuster les coordonnées GPS \n"
-            "3-Ajouter un logo \n"
-            "4-Revenir au choix de camera \n"
-            "5-Exit"
-        )
-        choix = int(input("Entrez le numero associé a l'action de votre choix: "))
-    if choix == 1:
-        mode_auto()
-    elif choix == 2:
-        change_gps_2.main()
-        menu_go_pro()
-    elif choix == 3:
-        loc_photos = None
-        while loc_photos not in [1, 2]:
-            print("1- img_to_be_changed/ \n" "2- img_to_be_changed/changed/")
-            loc_photos = int(
-                input("Entrez le numero associé au chemin de vos photos: ")
-            )
-        if loc_photos == 1:
-            path = "img_to_be_changed/"
-            # auto_nadir.auto(path)
-        else:
-            # auto_nadir.auto(path)
-            path = "img_to_be_changed/changed/"
-        sp.run(["./script_logo_insta.sh", path])
-        menu_go_pro()
-    elif choix == 4:
-        menu()
-    else:
-        exit
+    while True:
+        print("\n--- MENU GOPRO MAX ---")
+        print("1- Mode auto")
+        print("2- Ajuster les coordonnées GPS")
+        print("3- Ajouter un logo")
+        print("4- Retour")
+        print("5- Quitter")
+
+        choix = get_user_choice("Votre choix: ", [1, 2, 3, 4, 5])
+
+        if choix == 1:
+            mode_auto()
+        elif choix == 2:
+            change_gps_2.main()
+        elif choix == 3:
+            add_logo()
+        elif choix == 4:
+            return
+        elif choix == 5:
+            sys.exit()
 
 
 def menu_insta():
-    choix = None
-    while choix not in [1, 2, 3, 4, 5]:
-        print(
-            "1-Ajuster les coordonnées GPS \n"
-            "2-Ajouter un logo \n"
-            "3-Revenir au choix de camera \n"
-            "4-Adapter l'image a panoramax \n"
-            "5-Exit"
-        )
-        choix = int(input("Entrez le numero associé a votre choix : "))
-    if choix == 1:
-        change_gps_2.main()
-        menu_insta()
-    elif choix == 2:
-        loc_photos = None
-        while loc_photos not in [1, 2]:
-            print("1- img_to_be_changed/ \n" "2- img_to_be_changed/changed/")
-            loc_photos = int(
-                input("Entrez le numero associé au chemin de vos photos: ")
-            )
-        if loc_photos == 1:
-            path = "img_to_be_changed/"
-            # auto_nadir.auto(path)
-            sp.run(["./script_logo_insta.sh", path])
-        else:
-            # auto_nadir.auto(path)
-            path = "img_to_be_changed/changed/"
-            sp.run(["./script_logo_insta.sh", path])
-        menu_insta()
-    elif choix == 3:
-        menu()
+    while True:
+        print("\n--- MENU INSTA360 ---")
+        print("1- Mode auto")
+        print("2- Ajuster les coordonnées GPS")
+        print("3- Ajouter un logo")
+        print("4- Retour")
+        print("5- Adapter l'image à Panoramax")
+        print("6- Quitter")
 
-    elif choix == 4:
-        loc_photos = None
-        while loc_photos not in [1, 2, 3]:
-            print(
-                "1- img_to_be_changed/ \n"
-                "2- img_to_be_changed/changed/ \n"
-                "3- img_logo/"
-            )
-            loc_photos = int(
-                input("Entrez le numero associé au chemin de vos photos: ")
-            )
-        if loc_photos == 1:
-            path = "img_to_be_changed/"
-        elif loc_photos == 2:
-            path = "img_to_be_changed/changed/"
-        elif loc_photos == 3:
-            path = "img_logo/"
-        sp.run(["./meta_insta.sh", path])
-        menu_insta()
-    else:
-        exit
+        choix = get_user_choice("Votre choix: ", [1, 2, 3, 4, 5, 6])
+
+        if choix == 1:
+            mode_auto()
+        elif choix == 2:
+            change_gps_2.main()
+        elif choix == 3:
+            add_logo()
+        elif choix == 4:
+            return
+        elif choix == 5:
+            adapt_meta_insta()
+        elif choix == 6:
+            sys.exit()
+
+
+def add_logo():
+    print("\n--- AJOUT DE LOGO ---")
+    print("1- img_to_be_changed/")
+    print("2- img_to_be_changed/changed/")
+
+    choix = get_user_choice("Choisissez le dossier: ", [1, 2])
+    path = "img_to_be_changed/" if choix == 1 else "img_to_be_changed/changed/"
+    sp.run(["./script_logo_insta.sh", path])
+
+
+def adapt_meta_insta():
+    print("\n--- MODIFICATION METADONNÉES ---")
+    print("1- img_to_be_changed/")
+    print("2- img_to_be_changed/changed/")
+    print("3- img_logo/")
+
+    choix = get_user_choice("Chemin: ", [1, 2, 3])
+    path = {1: "img_to_be_changed/", 2: "img_to_be_changed/changed/", 3: "img_logo/"}[
+        choix
+    ]
+    sp.run(["./meta_insta.sh", path])
 
 
 def mode_auto():
-    print(
-        "Bienvenue dans le mode auto cette option prendra tout en charge c'es à dire :\n"
-        "   -changement de coordonées gps \n"
-        "   -ajout d'un logo \n"
-        "   -modification des metadonné (si insta 360) \n"
-        "   -push sur panoramax \n"
-        "!!!! ATTENTION !!!! pour que ce programme se déroule bien veillez à bien avoir un fichier kml, les photos au format valide(2:1) et un logo"
-    )
+    global CAMERA
+
+    print("\n--- MODE AUTO ---")
+    print("Ce mode effectue automatiquement :")
+    print("  - Changement des coordonnées GPS")
+    print("  - Ajout de logo")
+    print("  - Modification des métadonnées (Insta360 uniquement)")
+    print("  - Upload vers Panoramax")
+    print("\nAssurez-vous que les fichiers KML, les images et le logo sont prêts.")
+
     kml_to_geojson.converter_kml()
     change_gps_2.main()
-    sp.run(["./meta_insta.sh", "img_to_be_changed/changed/"])
+
     if CAMERA == "Insta360 one x2":
         sp.run(["./meta_insta.sh", "img_logo/"])
+    else:
+        sp.run(["./meta_insta.sh", "img_to_be_changed/changed/"])
+
+    sp.run(["./script_logo_insta.sh", "img_to_be_changed/changed/"])
+
     sp.run(
         [
-            "panoramax_cli upload --api-url https://panoramax.openstreetmap.fr/",
+            "panoramax_cli",
+            "upload",
+            "--api-url",
+            "https://panoramax.openstreetmap.fr/",
             "img_logo/",
         ]
     )
 
 
-menu()
+if __name__ == "__main__":
+    print_banner()
+    menu()
